6b53f46f61b2255d1e6dc3f81d1918459b0fda51
Yaroslav Rakityanskiy <rakityanskiy@mercurydevelopment.com>
2022-07-05 11:16:37 +0400

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    plugins: [
+        new HtmlWebPackPlugin({
+            
+            template: './src/app/dvirPro.html',
+                        
+            filename: './dvirPro.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'dvirPro.js'
+    }
+}
