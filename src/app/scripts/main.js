5e1901d03eefb333aa834e3f35d30a1e409944af
Yaroslav Rakityanskiy <rakityanskiy@mercurydevelopment.com>
2022-07-05 12:33:21 +0400

    Test

--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -25,6 +25,7 @@ geotab.addin.dvirPro = function () {
     */
     startup: function (freshApi, freshState, initializeCallback) {
       // MUST call initializeCallback when done any setup
+      document.querySelector('#dashboard-dvir').remove();
       initializeCallback();
     },
     
@@ -39,6 +40,7 @@ geotab.addin.dvirPro = function () {
      *        for display to the user.
      */
     initialize: function (freshApi, freshState, initializeCallback) {
+      document.querySelector('#dashboard-dvir').remove();
       // Loading translations if available
       if (freshState.translate) {
         freshState.translate(elAddin || '');
@@ -59,6 +61,7 @@ geotab.addin.dvirPro = function () {
      * @param {object} freshState - The page state object allows access to URL, page navigation and global group filter.
     */
     focus: function (freshApi, freshState) {
+      document.querySelector('#dashboard-dvir').remove();
        // getting the current user to display in the UI
         freshApi.getSession(session => {
           freshApi.call('Get', {
@@ -89,6 +92,7 @@ geotab.addin.dvirPro = function () {
      * @param {object} freshState - The page state object allows access to URL, page navigation and global group filter.
     */
     blur: function () {
+      document.querySelector('#dashboard-dvir').remove();
       // hide main content
       elAddin.className += ' hidden';
     },
